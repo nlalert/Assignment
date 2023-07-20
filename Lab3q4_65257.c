c314fc0f101c6815b4d05b738a37171e09e502e4
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2023-07-20 16:07:03 +0700

    Update Lab3q4_65257.c

--- Lab3q4_65257.c
+++ Lab3q4_65257.c
@@ -2,16 +2,16 @@
 int main(){
 	int total = 0,input;
 	float i = 0;
-	printf("enter a number please : ");
+	printf("enter a number : ");
 	scanf("%d",&input);
 	while(input > 0){
 		total += input;
 		i++;	
-		printf("enter a number please : ");
+		printf("enter a number : ");
 		scanf("%d",&input);
 	}
 	if(i == 0) i++;
-	printf("Sum of your numbers are : %d\n",total);
-	printf("Average of your numbers are : %.2f\n",(total/i));
+	printf("Sum of your numbers is : %d\n",total);
+	printf("Average of your numbers is : %.2f\n",(total/i));
 	return 0;
 }
